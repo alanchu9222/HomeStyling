60c52a3e2748201414925cbb12e07ac86fa63581
Alan Chu <alan_chu@yahoo.com>
2019-06-04 14:05:14 +1000

    Initial baseline

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+#root to: "home#index"
+
+Rails.application.routes.draw do
+  get 'pages/home'
+  devise_for :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
