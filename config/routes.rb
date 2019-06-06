b1a8671fd6590fd4488236c4a5d461e173913d25
Alan Chu <alan_chu@yahoo.com>
2019-06-06 15:37:06 +1000

    Route directly to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 #root to: "home#index"
 
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
